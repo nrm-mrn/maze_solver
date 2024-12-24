c0203336932d0fadd96fe9925a7b6859633a807b
nicorum <nicorum@yandex.ru>
2024-12-24 21:45:06 +0500

    points, lines, cells and connecting centers

---
+++ geomentry.py
@@ -0,0 +1,97 @@
+from __future__ import annotations
+from tkinter import Canvas, Tk
+
+
+class Point:
+    def __init__(self, x: float, y: float) -> None:
+        self.x: float = x
+        self.y: float = y
+
+
+class Line:
+    def __init__(self, a: Point, b: Point) -> None:
+        self.a: Point = a
+        self.b: Point = b
+
+    def draw(self, canv: Canvas, fill_color: str) -> None:
+        canv.create_line(
+            self.a.x, self.a.y, self.b.x, self.b.y, fill=fill_color, width=2
+        )
+
+
+class Window:
+    def __init__(self, width: int, height: int) -> None:
+        self.root: Tk = Tk()
+        self.root.title("Maze solver")
+        self.canvas: Canvas = Canvas(self.root, width=width, height=height)
+        self.canvas.pack()
+        self.running: bool = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self) -> None:
+        self.canvas.update_idletasks()
+        self.canvas.update()
+
+    def wait_for_close(self) -> None:
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    def close(self) -> None:
+        self.running = False
+
+    def draw_line(self, line: Line) -> None:
+        line.draw(self.canvas, fill_color="red")
+
+
+class Cell:
+    """
+    positioning of the walls depends on coordinates: cell coords should be
+    specified from top left to bottom right, otherwise walls could flip sides
+    """
+
+    def __init__(
+        self,
+        x1: float,
+        x2: float,
+        y1: float,
+        y2: float,
+        window: Window,
+        has_left_wall: bool = True,
+        has_right_wall: bool = True,
+        has_top_wall: bool = True,
+        has_bottom_wall: bool = True,
+    ):
+        self._x1: float = x1
+        self._x2: float = x2
+        self._y1: float = y1
+        self._y2: float = y2
+        self._win: Window = window
+        self.has_left_wall: bool = has_left_wall
+        self.has_right_wall: bool = has_right_wall
+        self.has_top_wall: bool = has_top_wall
+        self.has_bottom_wall: bool = has_bottom_wall
+
+    def draw(self, fill_color: str = "white"):
+        if self.has_left_wall:
+            left: Line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            left.draw(self._win.canvas, fill_color)
+        if self.has_right_wall:
+            right: Line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            right.draw(self._win.canvas, fill_color)
+        if self.has_bottom_wall:
+            bottom: Line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            bottom.draw(self._win.canvas, fill_color)
+        if self.has_top_wall:
+            top: Line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            top.draw(self._win.canvas, fill_color)
+
+    def draw_move(self, to_cell: Cell, undo=False):
+        color: str = "red"
+        if undo:
+            color = "gray"
+        line: Line = Line(
+            Point((self._x1 + self._x2) / 2, (self._y1 + self._y2) / 2),
+            Point((to_cell._x1 + to_cell._x2) / 2, (to_cell._y1 + to_cell._y2) / 2),
+        )
+        line.draw(self._win.canvas, color)
---
+++ main.py
@@ -0,0 +1,74 @@
+from geomentry import Line, Point, Cell, Window
+
+
+def main() -> None:
+    win: Window = Window(800, 600)
+    cell_1: Cell = Cell(400, 410, 300, 310, win, has_bottom_wall=False)
+    cell_2: Cell = Cell(
+        400, 410, 310, 320, win, has_bottom_wall=False, has_top_wall=False
+    )
+    cell_3: Cell = Cell(
+        400, 410, 320, 330, win, has_bottom_wall=False, has_top_wall=False
+    )
+    cell_4: Cell = Cell(
+        400, 410, 330, 340, win, has_bottom_wall=False, has_top_wall=False
+    )
+    cell_5: Cell = Cell(
+        400,
+        410,
+        340,
+        350,
+        win,
+        has_top_wall=False,
+        has_right_wall=False,
+    )
+
+    cell_6: Cell = Cell(
+        410, 420, 340, 350, win, has_left_wall=False, has_right_wall=False
+    )
+    cell_7: Cell = Cell(
+        420, 430, 340, 350, win, has_left_wall=False, has_right_wall=False
+    )
+    cell_8: Cell = Cell(
+        430, 440, 340, 350, win, has_left_wall=False, has_top_wall=False
+    )
+    cell_9: Cell = Cell(
+        430, 440, 330, 340, win, has_bottom_wall=False, has_top_wall=False
+    )
+    cell_10: Cell = Cell(
+        430,
+        440,
+        320,
+        330,
+        win,
+        has_bottom_wall=False,
+    )
+    cell_1.draw()
+    cell_2.draw()
+    cell_3.draw()
+    cell_4.draw()
+    cell_5.draw()
+    cell_6.draw()
+    cell_7.draw()
+    cell_8.draw()
+    cell_9.draw()
+    cell_10.draw()
+    cell_1.draw_move(cell_2)
+    cell_2.draw_move(cell_3)
+    cell_3.draw_move(cell_4)
+    cell_4.draw_move(cell_5)
+    cell_5.draw_move(cell_6)
+    cell_6.draw_move(cell_7)
+    cell_7.draw_move(cell_8)
+    cell_8.draw_move(cell_9)
+    cell_9.draw_move(cell_10)
+
+    cell_10.draw_move(cell_9, undo=True)
+    cell_9.draw_move(cell_8, undo=True)
+    cell_8.draw_move(cell_7, undo=True)
+    cell_7.draw_move(cell_6, undo=True)
+    win.wait_for_close()
+
+
+if __name__ == "__main__":
+    main()
